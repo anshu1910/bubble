8f4f651b6b692895f6fbed61a6490ff5bb621c57
Anshu Soni <98822100+anshu1910@users.noreply.github.com>
2023-12-28 22:22:28 +0530

    added partials and layouts

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts= require('express-ejs-layouts');
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 app.use('/', require('./routes'));
 
 app.listen(port, function(err){
@@ -10,3 +18,5 @@ app.listen(port, function(err){
     }
     console.log('Server is up and running at port: ',port);
 });
+
+
