58f51b922c67742e07c3ad8d11b355e3da23a534
Anshu Soni <98822100+anshu1910@users.noreply.github.com>
2023-12-30 04:01:55 +0530

    Commiting in local-auth branch

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,27 @@
 const User = require('../models/user');
 
-module.exports.profile = function(req, res){
-    return res.render('user-profile', {
-        title: "Profile | Bubble"
-    });
+module.exports.profile = async function(req, res){
+    try {
+        if(req.cookies.user_id){
+            const user = await User.findById(req.cookies.user_id).exec();
+            
+            if(user){
+                return res.render('user-profile', {
+                    title: "User Profile",
+                    user: user
+                });
+            } else {
+                return res.redirect('/users/sign-in');
+            }
+        } else {
+            return res.redirect('/users/sign-in');
+        }
+    } catch (err) {
+        console.log('Error fetching user profile:', err);
+        return res.status(500).send('Internal Server Error');
+    }
 }
+
 
 module.exports.signUp = function(req, res){
     return res.render('user-sign-up', {
@@ -39,6 +56,31 @@ module.exports.create = async function(req, res){
     }
 }
 
-module.exports.createSession = function(req, res){
-    // To be implemented later
+//const User = require('../models/user');
+
+module.exports.createSession = async function(req, res){
+    try {
+        const foundUser = await User.findOne({ email: req.body.email }).exec();
+
+        if(foundUser){
+            if(foundUser.password !== req.body.password){
+                return res.redirect('back');
+            }
+            res.cookie('user_id', foundUser.id);
+            return res.redirect('/users/profile');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log('Error in signing in:', err);
+        return res.status(500).send('Internal Server Error');
+    }
 }
+
+module.exports.signOut = function(req, res) {
+    // Clear the user's session-related data
+    res.clearCookie('user_id'); // Assuming the user_id is stored in a cookie
+
+    // Redirect the user to a suitable page after sign-out
+    return res.redirect('/users/sign-in'); // Redirect to sign-in page or any other page
+};
