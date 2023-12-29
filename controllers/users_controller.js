5fdf9f2416e8c06ee2fcaf8228177ba2de9d9bab
Anshu Soni <98822100+anshu1910@users.noreply.github.com>
2023-12-30 03:26:26 +0530

    Setting up Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,44 @@
-module.exports.profile= function(req,res){
-    //res.end('<h1>User Profile</h1>');
-    return res.render('user_profile',
-    {
-        title:"Profile|Bubble"
+const User = require('../models/user');
+
+module.exports.profile = function(req, res){
+    return res.render('user-profile', {
+        title: "Profile | Bubble"
     });
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('user-sign-up', {
+        title: "Bubble | Sign Up"
+    });
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user-sign-in', {
+        title: "Bubble | Sign In"
+    });
+}
+
+module.exports.create = async function(req, res){
+    // Validate password and confirm-password
+    if(req.body.password !== req.body['confirm-password']) {
+        return res.redirect('back');
+    }
+
+    try {
+        const foundUser = await User.findOne({ email: req.body.email }).exec();
+
+        if(!foundUser){
+            const newUser = await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log('Error:', err);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
+module.exports.createSession = function(req, res){
+    // To be implemented later
 }
--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts= require('express-ejs-layouts');
+const db = require('./config/mongoose');
+const cookieParser = require('cookie-parser');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-app.use(express.static('./assets'));
-app.use(expressLayouts);
 app.use('/', require('./routes'));
 
 app.listen(port, function(err){
