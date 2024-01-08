d37f3373de81cac419ed8670fe885901d61edb04
Anshu Soni <98822100+anshu1910@users.noreply.github.com>
2024-01-09 00:15:22 +0530

    redis and kue added

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function (req, res) {
     try {
@@ -23,8 +25,15 @@ module.exports.create = async function (req, res) {
       comment = await comment.populate('user', 'name email');
   
       // Send notification email for the new comment
-      commentsMailer.newComment(comment);
-  
+      
+      let job = queue.create('emails', comment).save(function(err){
+        if(err){
+          console.log('error in sending to the queue', err);
+          return;
+        }
+        console.log('job enqueued', job.id);
+      })
+
       if (req.xhr) {
         // If it's an XHR request, send a JSON response
         return res.status(200).json({
